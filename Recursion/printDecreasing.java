542faf927a4979c4478764350c96fe283206465a
Ehtesham Ahmad <109310046+thisisehtesham@users.noreply.github.com>
2022-09-20 03:31:23 +0530

    Add files via upload

---
+++ Recursion/printDecreasing.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+import java.io.*;
+import java.util.*;
+
+public class printDecreasing {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printDecreasing(n);       
+    }
+
+    public static void printDecreasing(int n){
+        if(n == 0) {
+            return;
+        }
+        System.out.println(n);
+        printDecreasing(n-1);
+    }
+
+}
